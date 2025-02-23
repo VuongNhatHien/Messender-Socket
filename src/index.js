7437fe50b44d953828bc99db1768da9449584769
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-23 16:50:34 +0700

    Add more logic

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
-require('dotenv').config(); 
-const express = require('express');
-const { createServer } = require('node:http');
-const { Server } = require('socket.io');
+require("dotenv").config();
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-        credentials: true,
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("joinChat", (chatId) => {
+    socket.join(chatId);
+    console.log(`User ${socket.id} joined chat ${chatId}`);
+  });
+
+  socket.on("sendMessage", ({ chatId, message }) => {
+    console.log(`Message received in chat ${chatId}: ${message}`);
+    io.to(chatId).emit("receiveMessage", message);
+  });
+
+  socket.on("leaveChat", (chatId) => {
+    socket.leave(chatId);
+    console.log(`User ${socket.id} left chat ${chatId}\n`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected: ${socket.id}\n`);
   });
 });
 
 server.listen(process.env.PORT, () => {
-  console.log('server running at http://localhost:4444');
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
