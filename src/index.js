961d505019c8830d1872d90d877d864a0ca3ebaa
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-23 13:16:32 +0700

    Fix cors error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config(); 
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
 
 const app = express();
@@ -8,12 +8,13 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+        credentials: true,
     },
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
   socket.on('disconnect', () => {
     console.log('A user disconnected:', socket.id);
   });
