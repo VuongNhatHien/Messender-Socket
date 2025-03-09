ed1e905f73e01b4cc6d0fa6d83b8879d4517673f
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-03-09 23:04:07 +0700

    Add cors for 192.168.1.4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://192.168.1.4:3000"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   },
